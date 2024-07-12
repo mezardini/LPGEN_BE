23078de5dd5c4384e9e4706ead4e692f9dcc205a
mezeiser <mezardini@gmail.com>
2024-07-12 21:20:00 +0100

    changed allowed hosts

--- lpgen_be/settings.py
+++ lpgen_be/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['partial-marti-redditleadhq-5cacd692.koyeb.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
