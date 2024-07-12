77be25ccf14fa08db2d9db8c114884773121ff9b
mezeiser <mezardini@gmail.com>
2024-07-12 19:51:21 +0100

    preparing for deployment

--- lpgen_be/settings.py
+++ lpgen_be/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['partial-marti-redditleadhq-5cacd692.koyeb.app', '127.0.0.1']
 
 
 # Application definition
